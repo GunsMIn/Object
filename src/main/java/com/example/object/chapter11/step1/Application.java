4c52b3bab98d57265b143f9d3a2d8cd86f28e266
medneter <medneter@naver.com>
2024-09-05 10:12:28 +0900

    Composition 코드 추가

---
+++ src/main/java/com/example/object/chapter11/step1/Application.java
@@ -0,0 +1,12 @@
+package com.example.object.chapter11.step1;
+
+import java.util.Arrays;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Lecture lecture = new Lecture("객체지향 프로그래밍",70,  Arrays.asList(81, 95, 75, 50, 45));
+        lecture.evaluate();
+
+    }
+}
---
+++ src/main/java/com/example/object/chapter11/step2/Professor.java
@@ -0,0 +1,17 @@
+package com.example.object.chapter11.step2;
+
+public class Professor {
+
+    private String name;
+    private Lecture lecture;
+
+    public Professor(String name, Lecture lecture) {
+        this.name = name;
+        this.lecture = lecture;
+    }
+
+    public String compileStatistics() {
+        return String.format("[%s] %s - Avg: %.1f", name,
+                lecture.evaluate(), lecture.average());
+    }
+}
